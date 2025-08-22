0168dd4682b9abd97cf4103f59d0817abebe012f
monish patil <monishpatil776@gmail.com>
2025-08-23 01:32:42 +0530

    Update download_similarity.py

--- download_similarity.py
+++ download_similarity.py
@@ -1,5 +1,5 @@
-import gdown
+# import gdown
 
-url = "https://drive.google.com/file/d/19jZMuhfHwXZrDr1XHH-2h0bKPXTkwoU_/view?usp=drive_link"
-output = "similarity.pkl"
-gdown.download(url, output, quiet=False)
+# url = "https://drive.google.com/file/d/19jZMuhfHwXZrDr1XHH-2h0bKPXTkwoU_/view?usp=drive_link"
+# output = "similarity.pkl"
+# gdown.download(url, output, quiet=False)
